9cdb8e65a0609b731fa933d3cb8cae086828d0ba
mofath <mohamed.elgaedi@gmail.com>
2021-07-05 07:14:48 +0200

    [server]: Added AuthController

---
+++ server/src/controllers/AuthController.js
@@ -0,0 +1,45 @@
+const { AuthService } = require('../services');
+const jwtUtils = require('../utils/jwt');
+const config = require('../config/config');
+
+
+async function create(req, res, next) {
+    try {
+        const user = await AuthService.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            user
+        });
+
+    } catch (err) {
+        throw new Error(err.message);
+    }
+}
+
+async function login(req, res, next) {
+    try {
+        const user = await AuthService.login(req.body);
+
+        const accessToken = jwtUtils.generateAccessToken(user.id, user.role);
+        const refreshToken = jwtUtils.generateRefreshToken(user.id);
+
+        res.cookie('access_token', accessToken, { httpOnly: true, sameSite: true });
+        res.cookie('refresh_token', refreshToken, { httpOnly: true, sameSite: true });
+
+        return res.status(200).json({
+            message: `Welcome, ${user.username}`,
+            isAuthenticated: true,
+            userInfo: user
+        });
+
+    } catch (error) {
+        next(new Error)
+    }
+};
+
+
+module.exports = {
+    create,
+    login,
+};
