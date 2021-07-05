59cd83fa79dc9f03070e49bfac2b66bb5c024ff9
mofath <mohamed.elgaedi@gmail.com>
2021-07-06 01:52:47 +0200

    [client]: code clean up

--- server/src/socket/events.js
+++ server/src/socket/events.js
@@ -1,6 +1,11 @@
 module.exports = (io) => {
 
     io.on('connection', (socket) => {
-        console.log('New connection', socket.id)
+        console.log('New connection', socket.id);
+
+        socket.on('addTask', (task) => {
+            console.log(222222222222222222222222222);
+            // console.log('socketData: ' + JSON.stringify(task));
+        });
     });
 }
