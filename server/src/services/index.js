ed22dd7812f8ad7d34e7403f73fcdb2f7a4888fd
mofath <mohamed.elgaedi@gmail.com>
2021-07-06 01:26:49 +0200

    [server]: Added router index

---
+++ server/src/services/index.js
@@ -0,0 +1,10 @@
+const AuthService = require('./auth');
+const UserService = require('./user');
+const TaskService = require('./task')
+
+
+module.exports = {
+    AuthService,
+    UserService,
+    TaskService
+};
