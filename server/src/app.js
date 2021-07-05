c5e18f7e7c54b8afc680ee718c1af5348c2088f9
mofath <mohamed.elgaedi@gmail.com>
2021-07-05 04:30:18 +0200

    [server]: split application logic from the server

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+// parse json request body
+app.use(express.json());
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
