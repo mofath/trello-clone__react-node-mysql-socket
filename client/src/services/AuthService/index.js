8c85751e9b2cf3dc1ab14319e3e7267f52fc1e93
mofath <mohamed.elgaedi@gmail.com>
2021-07-06 01:12:31 +0200

    [client]: removed proxy and added server uri

--- client/src/services/AuthService/index.js
+++ client/src/services/AuthService/index.js
@@ -1,17 +1,17 @@
 const AuthService = (httpClient) => {
 
     const signup = async (signupData) => {
-        const { data } = await httpClient.post('api/v1/auth/signup', signupData);
+        const { data } = await httpClient.post('http://localhost:3001/api/v1/auth/signup', signupData);
         return data;
     };
 
     const login = async (loginData) => {
-        const { data } = await httpClient.post('api/v1/auth/login', loginData);
+        const { data } = await httpClient.post('http://localhost:3001/api/v1/auth/login', loginData);
         return data;
     };
 
     const authenticate = async (loginData) => {
-        const { data } = await httpClient.get('api/v1/auth/token', loginData);
+        const { data } = await httpClient.get('http://localhost:3001/api/v1/auth/token', loginData);
         return data;
     };
 
