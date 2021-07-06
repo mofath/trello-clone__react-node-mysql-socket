1d6a652c3ca6f638989cbcfcbb0f885050407549
mofath <mohamed.elgaedi@gmail.com>
2021-07-06 13:22:31 +0200

    [server]: updated seed files

--- server/src/seeders/20210705221511-client-user.js
+++ server/src/seeders/20210705221511-client-user.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [{
-      id: 'a741dfa6-24de-4b46-afc7-370772f068d8',
+      id: 'a771dfa9-24de-4b46-afc7-370772f068d8',
       username: 'client',
-      password: '123456789',
+      password: '12345678',
       role: 'client',
       createdAt: '2020-01-01 10:10:10',
       updatedAt: '2020-01-01 10:10:10',
--- server/src/seeders/20210705221819-demo-user.js
+++ server/src/seeders/20210705221819-demo-user.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [{
-      id: 'a741dfa6-24de-4b46-afc7-370772f068d5',
+      id: 'a741dfa8-24de-4b46-afc7-370772f068d5',
       username: 'demo',
-      password: '123456789',
+      password: '12345678',
       createdAt: '2020-01-01 10:10:10',
       updatedAt: '2020-01-01 10:10:10',
     }]);
