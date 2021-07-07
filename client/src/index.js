130dfe4b84616e662e5869801521b345d2212286
mofath <mohamed.elgaedi@gmail.com>
2021-07-07 02:53:32 +0200

    [client]: code cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import App from './App';
-import { SocketContextProvider } from "./context";
+import { SocketContextProvider } from "./socket";
 import { store } from "./state";
-
-
 import './index.scss';
 
 ReactDOM.render(
