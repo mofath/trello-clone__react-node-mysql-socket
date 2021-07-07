42e7cde2ba5ceec4a3706f2394d2565ddc33d316
mofath <mohamed.elgaedi@gmail.com>
2021-07-07 13:11:48 +0200

    [client]:  Updated socket event constants

--- client/src/socket/events_names.js
+++ client/src/socket/events_names.js
@@ -1,3 +1,7 @@
 exports.CONNECTION = "connection";
+
 exports.ADD_TASK = "ADD_TASK";
+exports.TASK_ADDED = "TASK_ADDED";
+
 exports.UPDATE_TASK = "UPDATE_TASK";
+exports.TASK_UPDATED = "TASK_UPDATED";
