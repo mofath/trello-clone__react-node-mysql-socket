4919d7623e4a96d53225e1a085f2b62648f9d2c5
mofath <mohamed.elgaedi@gmail.com>
2021-07-07 13:12:51 +0200

    [server]: Implemented AddTask and UpdateTask events

--- server/src/socket/events.js
+++ server/src/socket/events.js
@@ -1,16 +1,29 @@
 const eventNames = require('./event_names');
+const { TaskService } = require('../services');
 
 module.exports = (io) => {
 
     io.on(eventNames.CONNECTION, (socket) => {
         console.log('New connection', socket.id);
 
-        socket.on(eventNames.ADD_TASK, (task) => {
+        socket.on(eventNames.ADD_TASK, async (task) => {
             console.log('socketData: ' + JSON.stringify(task));
+            try {
+                const result = await TaskService.create(task);
+                io.emit(eventNames.TASK_ADDED, result)
+            } catch (error) {
+                console.log(error.message);
+            }
         });
 
-        socket.on(eventNames.UPDATE_TASK, (data) => {
+        socket.on(eventNames.UPDATE_TASK, async (data) => {
             console.log('socketData: ' + JSON.stringify(data));
+            try {
+                const result = await TaskService.updateTaskStatus(data);
+                io.emit(eventNames.TASK_UPDATED, result)
+            } catch (error) {
+                console.log(error.message);
+            }
         });
     });
 }
