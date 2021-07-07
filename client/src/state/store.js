0854188aa787fa086a2ffc2cd163e85062907126
mofath <mohamed.elgaedi@gmail.com>
2021-07-07 03:25:08 +0200

    [client]: Added redux store composeEnhancers

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,5 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import {
+    compose,
+    createStore,
+    applyMiddleware,
+} from "redux";
 import thunk from "redux-thunk";
 import reucers from "./reducer";
 
-export const store = createStore(reucers, {}, applyMiddleware(thunk));
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+
+export const store = createStore(
+    reucers,
+    {},
+    composeEnhancers(applyMiddleware(thunk))
+);
