e6e9297d501ef5f309184d916ebca90533166b76
mofath <mohamed.elgaedi@gmail.com>
2021-07-07 02:56:18 +0200

    [client]: Re-styled TrelloList component

--- client/src/components/TrelloList/index.jsx
+++ client/src/components/TrelloList/index.jsx
@@ -3,19 +3,33 @@ import { Droppable } from 'react-beautiful-dnd';
 import TrelloCard from '../TrelloCard';
 import './index.scss';
 
-export default function TrelloList({ title, cards }) {
+export default function TrelloList({ title, cards = [], id }) {
   return (
-    <Droppable droppableId={title}>
+    <Droppable droppableId={title.toLowerCase()}>
       {(provided) => (
         <div
           {...provided.droppableProps}
           ref={provided.innerRef}
           className="trello-list"
         >
-          <h4>{title}</h4>
-          {cards.map((card, index) => (
-            <TrelloCard text={card.text} id={card.id} index={index} />
-          ))}
+          <h4 className="trello-list__title">{title}</h4>
+          {
+            cards?.length > 0 ?
+            cards.map((card, index) => (
+              <TrelloCard
+                title={card.title}
+                text={card.description}
+                id={card.id}
+                index={index}
+                listID={id}
+                key={card.id}
+              />
+            ))
+            :
+            <div className="trello-list__empty-indicator">
+              List has no cards
+            </div>
+          }
           {provided.placeholder}
         </div>
       )}
